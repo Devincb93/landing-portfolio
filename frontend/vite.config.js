374bd97ed1879f51e00f45fbbf62747573393927
Devin <devincb93@gmail.com>
2024-09-11 23:46:50 -0700

    interlopated site into deployable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"]
-    }
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
