b4e4b62f5eebd4dd2d2540d4693e1cd433828301
Devin <devincb93@gmail.com>
2025-07-16 19:02:25 -0700

    added better transition from nopage

--- frontend/src/NoPage.jsx
+++ frontend/src/NoPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
 
 const NoPage = () => {
+
+  useEffect(() => {
+    gsap.to("#container", {
+      opacity: 1,
+      duration: 1,
+    })
+  })
   return (
-    <div className='w-full h-screen flex items-center justify-center px-4 text-center'>
+    <div id='container' className='opacity-0 w-full h-screen flex items-center justify-center px-4 text-center'>
       <h1 className='text-3xl'>Great cause we're still under construction! Sorry about that but you can always check back with us in a few days and there should be some updates even if they're small. Thanks!</h1>
     </div>
   )
