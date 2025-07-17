c2626fc4f160a6772069970e679970dc21953d73
Devin <devincb93@gmail.com>
2025-07-16 20:11:05 -0700

    added simple transitions to yes page and about section will start working on layout of text

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
 
 const MainPage = () => {
+
+    useEffect(()=> {
+        gsap.to('#container', {
+            opacity:1,
+            duration:1,
+            fadeIn: true,
+        })
+    })
   return (
-    <div>MainPage</div>
+    <div id='container' className='opacity-0 w-full h-screen'>
+        <div>
+            <p>Hi, I’m Devin Burkett, a self-driven full stack software engineer based in Los Angeles. I specialize in building real-world web applications from the ground up, combining strong front-end skills in React and Tailwind CSS with solid back-end expertise in Node.js, Express, and Python. I have hands-on experience creating responsive, performant apps, including projects featuring 3D visuals with Three.js and robust real-time functionality.</p>
+            <p>My background includes problem-solving under pressure, gained from diverse roles in technical support and lab work, which has sharpened my ability to troubleshoot and deliver stable, high-quality software. I’m passionate about continuous learning and applying best practices in UI/UX design and clean architecture to create intuitive user experiences.</p>
+            <p>Currently, I’m looking to grow long-term with a team where I can contribute my skills and keep leveling up as a developer.</p>
+        </div>
+    </div>
   )
 }
 
