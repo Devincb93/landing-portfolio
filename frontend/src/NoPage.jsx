21335ecebe78676eeac07027609f7d0c7dbddb10
Devin <devincb93@gmail.com>
2025-07-16 20:31:07 -0700

    grammar error

--- frontend/src/NoPage.jsx
+++ frontend/src/NoPage.jsx
@@ -11,7 +11,7 @@ const NoPage = () => {
   })
   return (
     <div id='container' className='opacity-0 w-full h-screen flex items-center justify-center px-4 text-center'>
-      <h1 className='text-3xl'>Great cause we're still under construction! Sorry about that but you can always check back with us in a few days and there should be some updates even if they're small. Thanks!</h1>
+      <h1 className='text-3xl'>Great, cause we're still under construction! Sorry about that but you can always check back with us in a few days and there should be some updates even if they're small. Thanks!</h1>
     </div>
   )
 }
